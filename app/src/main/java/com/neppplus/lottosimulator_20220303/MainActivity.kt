1111db84092916072099784aadc29e49efa22800
nepp.plus <nepp.plus@gmail.com>
2022-03-03 10:49:14 +0900

    ArrayList로 당첨번호 목록 변수 추가, 실제 당첨번호 목록 저장 기능

--- app/src/main/java/com/neppplus/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottosimulator_20220303/MainActivity.kt
@@ -8,6 +8,10 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    컴퓨터가 뽑은 당첨번호 6개를 저장할 ArrayList
+    val mWinNumberList = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,6 +34,11 @@ class MainActivity : AppCompatActivity() {
 //        6개의 당첨번호 생성
 //        코틀린의 for문은, for-each 문법으로 기반.
 
+//        ArrayList는 목록을 계속 누적 가능.
+//        당첨번호 뽑기 전에, 기존의 당첨번호는 전부 삭제하고 다시 뽑자.
+
+        mWinNumberList.clear()
+
         for ( i  in 0 until 6  ) {
 
 //            괜찮은 번호가 나올때 까지 무한 반복
@@ -38,10 +47,10 @@ class MainActivity : AppCompatActivity() {
 //                1 ~ 45의 랜덤 숫자
 //                Math.random()은 0~1 => 1 ~ 45.xxx 로 가공 => Int로 캐스팅
                 val randomNum =  (Math.random() * 45 + 1).toInt()
-
 //                중복 검사 통과 시 while 깨자
-
-                if (true) {
+                if ( !mWinNumberList.contains(randomNum) ) {
+//                    당첨번호로, 뽑은 랜덤 숫자 등록
+                    mWinNumberList.add( randomNum )
                     break
                 }
 
@@ -50,6 +59,12 @@ class MainActivity : AppCompatActivity() {
         }
 
 //        만들어진 당첨번호 6개를 -> 텍스트뷰에 표현
+
+        Log.d("당첨번호목록", mWinNumberList.toString())
+
+        for (winNum  in  mWinNumberList ) {
+
+        }
         
 //        보너스번호 생성
         
