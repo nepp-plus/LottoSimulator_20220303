bc511101da5eaa249a4474d8681ba9c93436be60
nepp.plus <nepp.plus@gmail.com>
2022-03-03 14:53:05 +0900

    구매 시도시, 사용 금액 증가 / 당첨금액 누적  - 화면에 표시 X

--- app/src/main/java/com/neppplus/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottosimulator_20220303/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.measureNanoTime
 
 class MainActivity : AppCompatActivity() {
     
@@ -22,6 +23,12 @@ class MainActivity : AppCompatActivity() {
 
 //    당첨번호를 보여줄 6개의 텍스트뷰를 담아둘 ArrayList
     val mWinNumTextViewList = ArrayList<TextView>()
+
+
+//    사용한 금액, 당첨된 금액 합산 변수
+    var mUsedMoney = 0
+    var mEarnMoney = 0L  // 30억 이상의 당첨 대비. Long 타입으로 설정.
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +48,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun buyLotto() {
+
+//        사용한 금액 늘려주기
+        mUsedMoney += 1000
+
 
 //        6개의 당첨번호 생성
 //        코틀린의 for문은, for-each 문법으로 기반.
@@ -128,27 +139,26 @@ class MainActivity : AppCompatActivity() {
 
         when (correctCount) {
             6 -> {
-                Toast.makeText(this, "1등입니다.", Toast.LENGTH_SHORT).show()
+//                30억을 번 금액으로 추가
+                mEarnMoney += 3000000000
             }
             5 -> {
 //                보너스 번호를 맞췄는지? => 보너스번호가 내 번호 목록에 들어있나?
 
                 if (mMyNumbers.contains(mBonusNum)) {
-                    Toast.makeText(this, "2등입니다.", Toast.LENGTH_SHORT).show()
+                    mEarnMoney += 50000000
                 }
                 else {
-                    Toast.makeText(this, "3등입니다.", Toast.LENGTH_SHORT).show()
+                    mEarnMoney += 2000000
                 }
 
             }
             4 -> {
-                Toast.makeText(this, "4등입니다.", Toast.LENGTH_SHORT).show()
+                mEarnMoney += 50000
             }
             3 -> {
-                Toast.makeText(this, "5등입니다.", Toast.LENGTH_SHORT).show()
-            }
-            else -> {
-                Toast.makeText(this, "낙첨입니다.", Toast.LENGTH_SHORT).show()
+//                5등 -> 5천원을 사용한 돈을 줄여주자.
+                mUsedMoney -= 5000
             }
         }
 
